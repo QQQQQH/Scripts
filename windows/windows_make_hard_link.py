9ac9082acd8ee2f0d1300d5d243a2a6e99d3319d
Mituian <NatumeMituan@gmail.com>
2020-08-29 00:50:27 +0800

    add  windows_make_hard_link

---
+++ windows/windows_make_hard_link.py
@@ -0,0 +1,23 @@
+import os
+
+
+def makeHardLink(fromFile, toFile):
+    if not os.path.exists(toFile):
+        os.makedirs(toFile)
+    files = os.listdir(fromFile)
+    for f in files:
+        fromF = os.path.join(fromFile, f)
+        toF = os.path.join(toFile, f)
+        if os.path.isdir(fromF):
+            makeHardLink(fromF, toF)
+        else:
+            cmdStr = "mklink /H \""+toF+"\" \""+fromF+"\""
+            print(cmdStr)
+            p = os.popen(cmdStr)
+            print(p)
+
+
+if __name__ == '__main__':
+    sourcePath = input()
+    targetPath = os.getcwd()
+    makeHardLink(sourcePath, targetPath)
